92a6f68dee62776304e942db67b0f54b7d164c09
“Jared <jaredpdesigns@gmail.com>
2016-11-04 15:36:00 -1000

    :wrench: Improvements to Search
    - Added local copy of List.js
    - Added ‘sort’ icon and improved function of sorting search results

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -3,6 +3,7 @@
 
 {% include_relative _partials/typekit.js %}
 {% include_relative _partials/unveil.js %}
+{% include_relative _partials/list.js %}
 
 $(document).ready(function() {
   $('img').unveil(168,function() { $(this).load(function() { this.style.opacity = 1; } ); });
@@ -12,4 +13,11 @@ $(document).ready(function() {
 
 $(function() {
   $.stayInWebApp();
+});
+
+$(document).ready(function() {
+  var options = {
+    valueNames: [ 'book--title', 'book--author' ]
+  };
+  var userList = new List('booksearch', options);
 });
