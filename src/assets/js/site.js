0dc41d4f2e8d394de8e83973881f15bfe86d69cb
“Jared <jaredpdesigns@gmail.com>
2016-12-08 10:08:57 -1000

    :wrench: Javascript delivery improvements

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,27 +1,4 @@
 ---
 ---
 
-{% include_relative _partials/typekit.js %}
-{% include_relative _partials/unveil.js %}
-{% include_relative _partials/list.js %}
-
-$(document).ready(function() {
-  $('img').unveil(240,function() {
-    $(this).load(function() {
-      $(this).parent().addClass('loaded');
-    } );
-  });
-});
-
-{% include_relative _partials/stayInWebApp.js %}
-
-$(function() {
-  $.stayInWebApp();
-});
-
-$(document).ready(function() {
-  var options = {
-    valueNames: [ 'book__title', 'book__author' ]
-  };
-  var userList = new List('booksearch', options);
-});
+{% include_relative _partials/typekit.js %} {% include_relative _partials/unveil.js %} {% include_relative _partials/list.js %} $(document).ready(function() { $('img').unveil(240,function() { $(this).load(function() { $(this).parent().addClass('loaded'); } ); }); }); {% include_relative _partials/stayInWebApp.js %} $(function() { $.stayInWebApp(); }); $(document).ready(function() { var options = { valueNames: [ 'book__title', 'book__author' ] }; var userList = new List('booksearch', options); });
